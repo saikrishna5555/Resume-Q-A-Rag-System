93b45c82e1da9f425f44d9dd3351b2a9df07556f
saikrishna5555 <20mh1a04b5@acoe.edu.in>
2026-08-16 18:17:16 +0530

    added chunking

--- main_app/rag/chunks.py
+++ main_app/rag/chunks.py
@@ -2,12 +2,14 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from doc_loader import document
 def chunks():
     doc=document()
-    splitter=RecursiveCharacterTextSplitter(
+    splitter = RecursiveCharacterTextSplitter(
         chunk_size=500,
         chunk_overlap=50,
         separators=["\n\n","\n","."," ",""])
     chunk=splitter.split_documents(doc)
+
     return chunk
+
 
 if __name__=="__main__":
     chunks()
